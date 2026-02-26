a1380468b3a0b828bc6f3840c5be221273a9c4ac
Zain <116815294+Matrixszh@users.noreply.github.com>
2026-02-26 20:19:37 +0530

    Update page.tsx
    Video size dec

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -218,7 +218,7 @@ export default function AboutPage() {
                     loop
                     muted
                     playsInline
-                    className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-110"
+                    className="w-full h-[30vh] object-cover transition-transform duration-700 group-hover:scale-110"
                   />
                </div>
                
